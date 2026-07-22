526efa3b8ca8d0f241f676cf6656d20eec882716
ridameharin <ridameharinofficial@gmail.com>
2026-07-22 10:07:19 +0530

    Time: 1 ms (11.79%), Space: 54.4 MB (15.91%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.includes(target)){
+        return nums.indexOf(target)
+    }
+    else{
+        nums.push(target)
+        let sorted=nums.sort((a,b)=>a-b)
+        return nums.indexOf(target)
+    }
+};
