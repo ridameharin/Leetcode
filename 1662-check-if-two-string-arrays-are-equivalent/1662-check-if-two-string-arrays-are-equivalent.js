a9b078210c73b1f971ec451e4314f3f1d9ebc44c
ridameharin <ridameharinofficial@gmail.com>
2026-08-12 10:41:01 +0530

    Time: 0 ms (100%), Space: 53.9 MB (53.41%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    let a=word1.reduce((b,c)=>b+c,0)
+    let aa=word2.reduce((d,e)=>d+e,0)
+    if(a===aa){
+        return true;
+    }
+    return false
+};
